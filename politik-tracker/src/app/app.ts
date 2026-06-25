95328da41941bcb4ad5104f2f09a6e0e600e81a8
Emily Bennett <emily.pamelabennett@gmail.com>
2026-06-25 14:52:57 -0500

    background images force angular to not resolve

--- politik-tracker/src/app/app.ts
+++ politik-tracker/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { ResourceTracker } from './resource-tracker/resource-tracker';
 import { HeaderComponent } from './header-component/header-component';
 import { Resource } from './resource';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ResourceTracker, HeaderComponent],
+  imports: [ResourceTracker, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
