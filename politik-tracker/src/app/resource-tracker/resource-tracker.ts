f474b3e10d559e5362a2efedd716dc447d483d65
bennettem <emily.pamelabennett@gmail.com>
2026-06-26 14:43:01 -0500

    reverting counter and adding favicon

--- politik-tracker/src/app/resource-tracker/resource-tracker.ts
+++ politik-tracker/src/app/resource-tracker/resource-tracker.ts
@@ -12,39 +12,13 @@ export class ResourceTracker {
   selectedResource = input<Resource>();
   count = 0;
   maxLimit = 20000;
-  holdTimeout: any = null;
-  holdInterval: any = null;
-  isHolding = false;
-  readonly HOLD_DELAY = 400;
-  readonly HOLD_INTERVAL = 100;
 
   get imagePath() {
     const resource = this.selectedResource();
     return resource ? './assets/resources/' + resource.name + 'Icon.png' : '';
   }
 
-  //TODO: update counter in "real time" so a user can visually see how much it's jumping up by
-  startCounter(val: string) {
-    this.isHolding = false;
-    this.handleCounter(val, 1);
-
-    //delay
-    this.holdTimeout = setTimeout(() => {
-      this.isHolding = true;
-      this.holdInterval = setInterval(() => {
-        //change # based on how little/much you want to increament by
-        this.handleCounter(val, 5);
-      }, this.HOLD_INTERVAL);
-    }, this.HOLD_DELAY);
-  }
-
-  stopCounter() {
-    clearTimeout(this.holdTimeout);
-    clearInterval(this.holdInterval);
-    this.isHolding = false;
-  }
-
-  handleCounter(val: string, step: number = 1) {
+  handleCounter(val: string) {
     if (val == 'minus') {
       //number cannot be less than 0
       this.count = Math.max(this.count - 1, 0);
