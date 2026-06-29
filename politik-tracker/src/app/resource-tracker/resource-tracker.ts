517f87b9e72ae46df02145cdd41429e7dc47356c
Emily Bennett <emily.pamelabennett@gmail.com>
2026-06-29 10:03:33 -0500

    button style changes, adding signal set count to keep track of num increasing/decreasing and manifest paths changed

--- politik-tracker/src/app/resource-tracker/resource-tracker.ts
+++ politik-tracker/src/app/resource-tracker/resource-tracker.ts
@@ -1,6 +1,7 @@
 import { Component, input } from '@angular/core';
 import { Resource } from '../resource';
 import { FormsModule } from '@angular/forms';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-resource-tracker',
@@ -10,28 +11,51 @@ import { FormsModule } from '@angular/forms';
 })
 export class ResourceTracker {
   selectedResource = input<Resource>();
-  count = 0;
+  // count = 0;
+  count = signal(0);
   maxLimit = 20000;
+
+  //start: add mouse events
+  private holdTimeout: any;
+  private repeatInterval: any;
 
   get imagePath() {
     const resource = this.selectedResource();
     return resource ? './assets/resources/' + resource.name + 'Icon.png' : '';
   }
 
-  handleCounter(val: string) {
+  //adding safety net for handler
+  handleCounter(val: 'plus' | 'minus') {
     if (val == 'minus') {
       //number cannot be less than 0
-      this.count = Math.max(this.count - 1, 0);
+      this.count.set(Math.max(this.count() - 1, 0));
     } else {
-      if (val == 'plus') {
-        //number cannot be greater than set max limit
-        this.count = Math.min(this.count + 1, this.maxLimit);
-      }
+      //number cannot be greater than set max limit
+      this.count.set(Math.min(this.count() + 1, this.maxLimit));
     }
+  }
+
+  // active button hold counter
+  startCounter(val: 'plus' | 'minus') {
+    this.handleCounter(val);
+
+    // create a timeout
+    this.holdTimeout = setTimeout(() => {
+      this.repeatInterval = setInterval(() => {
+        this.handleCounter(val);
+      }, 75);
+    }, 400);
+  }
+
+  //stop counter after hold
+  stopCounter() {
+    clearTimeout(this.holdTimeout);
+    clearInterval(this.repeatInterval);
   }
 
   //keeps the user from typing over or under the limits set
   onInputChange() {
-    this.count = Math.min(Math.max(this.count, 0), this.maxLimit);
+    // this.count = Math.min(Math.max(this.count, 0), this.maxLimit);
+    this.count.set(Math.min(Math.max(this.count(), 0), this.maxLimit));
   }
 }
